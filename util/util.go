fd166de06b6b81f1d1c519cdb85600cc0c581c93
stormlin <lin-jinting@outlook.com>
2019-11-17 15:32:34 +0800

    Added the marshal and unmarshal method

--- util/util.go
+++ util/util.go
@@ -1,7 +1,10 @@
 package util
 
 import (
+  "encoding/json"
   "fmt"
+  "io/ioutil"
+  "os"
 )
 
 func Usage() {
@@ -20,4 +23,48 @@ func Usage() {
   fmt.Println("      [plan delete <plan_id>]")
   fmt.Println("    retitle: retitle an existing plan")
   fmt.Println("      [plan retitle <plan_id> 'a_new_title']")
+}
+
+// Entry for each plan item
+type PlanItem struct {
+  PlanId       string     `json:"plan_id"`
+  Date         string     `json:"date"`
+  Title        string     `json:"title"`
+  ChildrenPlan []PlanItem `json:"children_plan"`
+}
+
+// Root entry for plan list
+type PlanList struct {
+  MajorPlan []PlanItem `json:"major_plan"`
+}
+
+// Parse PlanList object from json file
+func ReadFromJsonFile(filePath string) (*PlanList, error) {
+  jsonFile, err := os.Open(filePath)
+  defer jsonFile.Close()
+  if err != nil {
+    return nil, err
+  }
+
+  byteValue, err := ioutil.ReadAll(jsonFile)
+  if err != nil {
+    return nil, err
+  }
+
+  var planList PlanList
+  err = json.Unmarshal(byteValue, &planList)
+  if err != nil {
+    return nil, err
+  }
+
+  return &planList, nil
+}
+
+func SaveAsJsonFile(filePath string, planList *PlanList) error {
+  file, err := json.MarshalIndent(planList, "", "  ")
+  if err != nil {
+    return err
+  }
+  err = ioutil.WriteFile(filePath, file, 0644)
+  return err
 }
--- util/util_test.go
+++ util/util_test.go
@@ -1,9 +1,46 @@
 package util
 
 import (
+  "fmt"
   "testing"
 )
 
 func TestUsage(t *testing.T) {
   Usage()
+}
+
+func TestSaveAsJsonFile(t *testing.T) {
+  planList := &PlanList{
+    MajorPlan: []PlanItem{
+      {
+        PlanId:       "plan-1",
+        Date:         "2018-01-01",
+        Title:        "title-1",
+        ChildrenPlan: nil,
+      },
+      {
+        PlanId:       "plan-2",
+        Date:         "2019-01-01",
+        Title:        "title-2",
+        ChildrenPlan: []PlanItem{
+          {
+            PlanId: "child-1",
+            Date:   "child-1-date",
+            Title:  "child-1-title",
+            ChildrenPlan: nil,
+          },
+          {
+            PlanId: "child-2",
+            Date:   "child-2-date",
+            Title:  "child-2-title",
+            ChildrenPlan: nil,
+          },
+        },
+      },
+    },
+  }
+  err := SaveAsJsonFile("data.json", planList)
+  if err != nil {
+    fmt.Println(err.Error())
+  }
 }
