b7aa9ca824361fc515b6a867bf522282ceb2e251
stormlin <lin-jinting@outlook.com>
2019-11-17 19:29:37 +0800

    Finished command <retitle>.

--- command/command_retitle.go
+++ command/command_retitle.go
@@ -1,6 +1,9 @@
 package command
 
-import "fmt"
+import (
+  "fmt"
+  "github.com/stormlin/plan-go/util"
+)
 
 type retitleCommand struct {
   args []string
@@ -17,5 +20,33 @@ func (command retitleCommand) usage() {
 }
 
 func (command *retitleCommand) Execute() error {
+  if len(command.args) != 2 {
+    return &util.WrongArgumentNumberError{}
+  }
+
+  planList, err := util.ReadFromJsonFile(util.DefaultFilePath)
+  if err != nil {
+    return err
+  }
+
+  var targetPlan *util.PlanItem
+  for _, plan := range planList.MajorPlan {
+    result := util.FindPlan(plan, command.args[0])
+    if result != nil {
+      targetPlan = result
+      break
+    }
+  }
+  if targetPlan == nil {
+    return &util.PlanNotExistsError{}
+  }
+
+  newTitle := command.args[1]
+  // mark it and its children as done
+  targetPlan.Title = newTitle
+  err = util.SaveAsJsonFile(util.DefaultFilePath, planList)
+  if err != nil {
+    return err
+  }
   return nil
 }
